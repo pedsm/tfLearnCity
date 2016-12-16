4c478cced5a255dd71f6e598b3574c1b50a265cc
Pedro Mendonca <peumendonca@gmail.com>
2016-12-16 07:32:56 +0000

    Initial commit

---
+++ cityNames.py
@@ -0,0 +1,46 @@
+# LSTM(Long short term memory) to generate city names
+from __future__ import absolute_import, division, print_function
+
+import os
+from six import moves
+import ssl
+import tflearn
+from tflearn.data_utils import *
+
+#Step 1 - Retrieve the data
+path = "US_cities.txt"
+if not os.path.isfile(path):
+    context = ssl._create_unverified_context();
+    moves.urllib.request.urlretrieve("https://raw.githubusercontent.com/tflearn/tflearn.github.io/master/resources/US_Cities.txt", path, context=context)
+
+maxlen = 20
+
+#vectorize the text file
+X, Y, char_idx = \
+    textfile_to_semi_redundant_sequences(path, seq_maxlen=maxlen, redun_step=3)
+
+#Create LSTM
+g = tflearn.input_data(shape=[None, maxlen, len(char_idx)])
+g = tflearn.lstm(g,512,return_seq=True)
+g = tflearn.dropout(g,0.5)
+g = tflearn.lstm(g,512)
+g = tflearn.dropout(g,0.5)
+g = tflearn.fully_connected(g, len(char_idx), activation='softmax')
+
+#generate cities
+m = tflearn.SequenceGenerator(g, dictionary=char_idx,
+                                seq_maxlen=maxlen,
+                                clip_gradients=5.0,
+                                checkpoint_path='model_us_cities')
+
+#training
+for i in range(40):
+    seed = random_sequence_from_textfile(path, maxlen)
+    m.fit(X, Y, validation_set = 0.1, batch_size=128,
+            n_epoch=1 ,run_id='us cities')
+    print("Testing")
+    print(m.generate(30,temperature=1.2,seq_seed=seed))
+    print("Testing")
+    print(m.generate(30,temperature=1.0,seq_seed=seed))
+    print("Testing")
+    print(m.generate(30,temperature=0.5,seq_seed=seed))
